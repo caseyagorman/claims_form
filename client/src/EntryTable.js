8eebd377927869b1c770e372c2175914f6fb11f2
Casey Gorman <caseyagorman@gmail.com>
2019-06-07 18:20:21 -0700

    fix if statement in entry table to include null entries

--- client/src/EntryTable.js
+++ client/src/EntryTable.js
@@ -10,25 +10,41 @@ class EntryTable extends Component {
   }
 
   displayEntries(items){
+    console.log("initial items", items)
     if (!items) {
       return <div>No items</div>
     }
-    let newObject = {}
+    // let newObject = {}
+    // let newItems = []
+    // for (let i=0; i< items.length; i++){
+    //   for (let key in items[i]){
+    //     if(items[i][key]===""){
+    //       newObject[key] = "not entered"
+    //     }
+    //     else{
+    //       newObject[key] = items[i][key]
+    //     }
+    //   }
+    //   newItems.push(newObject)
+    //   newObject = {}
+    // }
     let newItems = []
-    for (let i=0; i< items.length; i++){
-      for (let key in items[i]){
-        if(items[i][key]===""){
+    let newObject = {}
+    items.forEach(obj => {
+      Object.keys(obj).forEach(key => {
+        if(obj[key]===null || !obj[key].length){
           newObject[key] = "not entered"
         }
-        else{
-          newObject[key] = items[i][key]
+        else {
+          newObject[key] = obj[key] 
         }
-      }
+      });
       newItems.push(newObject)
       newObject = {}
-    }
+    });
 
-    
+    console.log("items now", newItems)
+    // return <div></div>
     return <EntryPage items={newItems} />
   }
 
